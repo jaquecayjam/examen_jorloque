071be842f0adf2034f00c7c86433828237ec0e97
jaquecayjam <114066906+jaquecayjam@users.noreply.github.com>
2023-11-24 18:39:22 +0100

    Update ejer1v1.sh

--- ejer1v1.sh
+++ ejer1v1.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-
 # Configuración inicial
 region="us-east-1"
 vpc_cidr_block="192.168.0.0/22"
-subnet_cidr_blocks[0]="192.168.0.0/23"
-subnet_cidr_blocks[1]="192.168.2.0/24"
-subnet_cidr_blocks[2]="192.168.3.0/25"
-subnet_cidr_blocks[3]="192.168.3.128/27"
+subnet_cidr_blocks=("192.168.0.0/23" "192.168.2.0/24" "192.168.3.0/25" "192.168.3.128/27")
 department_names=("Ingenieria" "Desarrollo" "Mantenimiento" "Soporte")
-
 # Crear VPC
 vpc_id=$(aws ec2 create-vpc --cidr-block $vpc_cidr_block --amazon-provided-ipv6-cidr-block --query 'Vpc.VpcId' --output text --region $region)
 echo "VPC creada con ID: $vpc_id"
-
 # Habilitar la resolución DNS y asignar un nombre a la VPC
 aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-support "{\"Value\":true}" --region $region
 aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-hostnames "{\"Value\":true}" --region $region
